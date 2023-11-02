3ae38f3bd87f5f880d588f65854e7430766c8aee
Lars W. Andersen <lars@factus.dk>
2023-11-02 07:42:57 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Introduction 
+Script to setup what is needed in wsl1 and wsl2
+
+# Getting Started
+First copy the ssh key from windows and set rights
+
+```bash
+mkdir .ssh
+cp -r /mnt/c/Users/lars/.ssh ~/.
+chmod 600 ~/.ssh/*
+```
+
+Then clone this repository using git+ssh and run the setup script.
+
+TODO: move this to an ansible script instead, except that we need to install ansible first. Not sure this is worth the trouble.
+
+
+
+he .ssh directory permissions should be 700 (drwx------).  The public key (.pub file) should be 644 (-rw-r--r--). The private key (id_rsa) on the client host, and the authorized_keys file on the server, should be 600 (-rw-------).
---
+++ wslsetup.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+
+#WSL2 Ubuntu clean install with zsh and oh my zsh
+
+# Prerequisite
+# mkdir ~/.ssh
+# cp /mnt/d/Documents/Diverse/keys/ssh/* .ssh/
+# chmod 600 .ssh/*
+
+sudo apt-get update && sudo apt-get full-upgrade -y
+sudo apt install mc inetutils-tools fzf bat -y
+
+## Maybe 1password cli
+
+#To use rancher-desktop with wsl docker (not needed for nerdctl and containerd)
+sudo addgroup --system docker
+sudo adduser $USER docker
+newgrp docker
+# And something needs to be done so $USER always runs in group `docker` on the `Ubuntu` WSL
+sudo chown root:docker /var/run/docker.sock   #Is this still needed when running rancher-desktop?
+sudo chmod g+w /var/run/docker.sock
+
+#zsh stuff
+sudo apt install zsh -y
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Setup syntax higlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+
+mkdir ~/source
+cd ~/source
+git clone git@github.com:agkozak/zsh-z.git ~/source/zsh-z
+git clone git@ssh.dev.azure.com:v3/Factus/Main/ansible ~/source/ansible
+# Get kubernetes context on cmd line
+sudo git clone https://github.com/jonmosco/kube-ps1.git ~/source/kube-ps1
+
+# AZ CLI
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+ln -s /etc/bash_completion.d/azure-cli ~/.oh-my-zsh/custom/az.zsh  #Auto completion
+
+#Python3 and pip3
+sudo apt install python3-pip
+pip3 install --upgrade ansible ansible-lint molecule
+
+#KubeCtx
+#KubeNs
+sudo git clone https://github.com/ahmetb/kubectx /opt/kubectx
+sudo ln -s /opt/kubectx/kubectx /usr/local/bin/kubectx
+sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
+
+#Flux
+curl -s https://fluxcd.io/install.sh | sudo bash
+
+
+git config --global --add safe.directory '*'
+git config --global user.name "Lars W. Andersen"
+git config --global user.email "lars@factus.dk"
+
+curl -sS https://webinstall.dev/k9s | bash
+
+# pipx
+python3 -m pip install --user pipx
+python3 -m pipx ensurepath
+sudo apt install python3.8-venv
+
+#jrnl
+pipx install jrnl
+# /mnt/d/Documents/jrnl/journal.txt
+# nano edit ~/.config/jrnl/jrnl.yaml  og angiv nano som editor
+
+
+####  To solve errors around unsecure kubeconfigs
+# On WSL Linux create a etc/wsl.conf   Then wsl --terminate ubuntu and wait 8 seconds
+
+[automount]
+enabled = true
+options = "metadata,umask=22,fmask=11"
+
+# Then change rights on the link
+chmod go-r /mnt/c/Users/lars/.kube/config
+chmod 700 /mnt/c/Users/Lars/.kube/config
+
+
+
+#Kubectl locally
+sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+sudo apt-get update
+sudo apt-get install -y kubectl
+
+######  Brew  #################
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"' >> /home/lars/.zprofile
+sudo apt-get install build-essential
+brew install gcc
