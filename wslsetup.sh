57390b9f0990f53d19a417bfde458c54721bc609
Lars W. Andersen <lars@factus.dk>
2024-02-03 00:47:00 +0100

    added vault

--- wslsetup.sh
+++ wslsetup.sh
@@ -55,6 +55,13 @@ sudo ln -s /opt/kubectx/kubens /usr/local/bin/kubens
 #Flux
 curl -s https://fluxcd.io/install.sh | sudo bash
 
+# Vault
+sudo apt update && sudo apt install gpg wget
+wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
+gpg --no-default-keyring --keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg --fingerprint
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update && sudo apt install vault
+
 git config --global --add safe.directory '*'
 git config --global user.name "Lars W. Andersen"
 git config --global user.email "lars@factus.dk"
