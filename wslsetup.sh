63bd4e028470723eef9d51f2daf28cc268169777
Lars W. Andersen <lars@factus.dk>
2025-02-09 14:16:50 +0100

    Update wslsetup.sh

--- wslsetup.sh
+++ wslsetup.sh
@@ -134,3 +134,16 @@ wget https://packages.microsoft.com/config/ubuntu/$repo_version/packages-microso
 sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 sudo apt update && sudo apt install -y dotnet-sdk-8.0
+
+
+
+# KREW for kubectl
+(
+  set -x; cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
